3722b18df80a7d1eee0485acb79653b82debefdd
Babz <Babz@Babz-Laptop.(none)>
2013-04-28 14:18:07 +0200

    file loader loads rubber duck

--- code/MadMummies/MadMummies/src/Game/Loader/FileLoader.cpp
+++ code/MadMummies/MadMummies/src/Game/Loader/FileLoader.cpp
@@ -22,55 +22,60 @@ FileLoader::~FileLoader()
 
 Scene* FileLoader::LoadScene(std::string sceneName)
 {
+	Scene* scene = new Scene();
+	//scene->SetScene(scene);
+
+	Mesh* mesh = new Mesh();
+	//mesh->SetScene(scene);
+
 	Assimp::Importer importer;
+	
+	Shader* shader = new Shader();
+	shader->SetVertexShaderPath(".\\resources\\shader\\SimpleColor.vert");
+	shader->SetFragmentShaderPath(".\\resources\\shader\\SimpleColor.frag");
+	mesh->SetShader(shader);
 
 	const aiScene* impScene = importer.ReadFile(".\\resources\\meshes\\rubberDuck.dae", aiProcess_Triangulate);
-	if(!impScene) {
+	if(impScene == NULL) {
 		std::cout << importer.GetErrorString() << std::endl;
 		exit(-1);
 	}
-
+	
 	unsigned int noOfVertices = impScene->mMeshes[0]->mNumVertices;
 
 	//create vertex position array
 	float* positionBuffer = new float[noOfVertices*3];
 	float* normalBuffer = new float[noOfVertices*3];
-	float* uvBuffer = new float[noOfVertices*3];
+	float* uvBuffer = new float[noOfVertices*2];
+
 	
-	for(int i = 0; i < noOfVertices; i++) {		
+	for(unsigned int i = 0; i < noOfVertices; i++) {		
 		positionBuffer[i*3] = impScene->mMeshes[0]->mVertices[i].x;
 		positionBuffer[i*3+1] = impScene->mMeshes[0]->mVertices[i].y;
 		positionBuffer[i*3+2] = impScene->mMeshes[0]->mVertices[i].z;
 		normalBuffer[i*3] = impScene->mMeshes[0]->mNormals[i].x;
 		normalBuffer[i*3+1] = impScene->mMeshes[0]->mNormals[i].y;
 		normalBuffer[i*3+2] = impScene->mMeshes[0]->mNormals[i].z;
-		uvBuffer[i*3] = impScene->mMeshes[0]->mTextureCoords[1][i].x;
-		uvBuffer[i*3+1] = impScene->mMeshes[0]->mTextureCoords[1][i].y;
-		uvBuffer[i*3+2] = impScene->mMeshes[0]->mTextureCoords[1][i].z;
+		uvBuffer[i*2] = impScene->mMeshes[0]->mTextureCoords[0][i].x;
+		uvBuffer[i*2+1] = impScene->mMeshes[0]->mTextureCoords[0][i].y;
 	}
 
-	aiMesh* newMesh = impScene->mMeshes[0];
+	//(float* positionBuffer, float* normalBuffer, unsigned int* indexBuffer, float* uvBuffer, unsigned int vertexCount, unsigned int indexCount)
+	VertexBufferObject* vbo = new VertexBufferObject(positionBuffer, normalBuffer, 0, uvBuffer, noOfVertices, 0);
+	mesh->SetVertexBufferObject(vbo);
 
-	Scene* scene = new Scene();
-
-	Mesh* mesh = new Mesh();
-	
-	mesh->SetVertexBufferObject(new VertexBufferObject(positionBuffer, normalBuffer, 0, uvBuffer, noOfVertices, 0));
-	
-	
-	Shader* shader = new Shader();
-	shader->SetVertexShaderPath(".\\resources\\shader\\SimpleColor.vert");
-	shader->SetFragmentShaderPath(".\\resources\\shader\\SimpleColor.frag");
-	mesh->SetShader(shader);
 	scene->AddChild(mesh);
 	
+	/*
 	Cube* cube = new Cube();
 	scene->AddChild(cube);
 
 	Light* light = new Light();
 	scene->AddChild(light);
+	*/
 
 	Camera* camera = new Camera();
+	//camera->SetScene(scene);
 	camera->SetViewport(0, 0, 500, 500);
 	scene->AddChild(camera);
 
